2f5ed97d990f4b7842a4714625a6f26942654a6b
axavier62 <xavierprince777@gmail.com>
2021-10-08 21:25:21 +0530

    password

--- src/org/test/Bank1.java
+++ src/org/test/Bank1.java
@@ -1,5 +1,17 @@
 package org.test;
 
-public class Bank1 {
+public class Bank1 { 
+	private void password() {
+    System.out.println(" password");
+	}
+	public static void main(String[] args) {
+		
+	}
 
+	
+	
+	
+
+	
 }
+
